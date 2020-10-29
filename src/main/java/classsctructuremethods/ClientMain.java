717f60646b8ea3aca9449a094ea7631cd699ca4e
Sanyi <hunited@gmail.com>
2020-10-29 17:28:02 +0100

    Metódus feladatok - Client és Note

---
+++ src/main/java/classsctructuremethods/ClientMain.java
@@ -0,0 +1,16 @@
+package classsctructuremethods;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        Client client = new Client();
+
+        client.setName("Teszt Elek");
+        client.setYear(1980);
+        client.setAddress("Gödöllő Fő utca 1");
+
+        System.out.println("Név: "+client.getName()+"\nSzületett: "+client.getYear()+"\nLakcím: "+client.getAddress());
+
+        client.migrate("Gödöllő, Babati utca 17");
+        System.out.println("Az új címe: "+client.getAddress());
+    }
+}
---
+++ src/main/java/classsctructuremethods/NoteMain.java
@@ -0,0 +1,24 @@
+package classsctructuremethods;
+
+import java.util.Scanner;
+
+public class NoteMain {
+    public static void main(String[] args) {
+        Note note = new Note();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Neved: ");
+        String name = scanner.nextLine();
+        note.setName(name);
+
+        System.out.println("Jegyzet témája: ");
+        String topic = scanner.nextLine();
+        note.setTopic(topic);
+
+        System.out.println("Jegyzeted: ");
+        String text = scanner.nextLine();
+        note.setText(text);
+
+        System.out.println(note.getNoteText());
+    }
+}
