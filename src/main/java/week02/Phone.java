11866e88953634241f4593c548ca5b6fa4891346
Sanyi <hunited@gmail.com>
2020-11-03 10:21:44 +0100

    Phone gyakorló feladat feltöltése felhasználói adatbekéréssel

--- src/main/java/week02/Phone.java
+++ src/main/java/week02/Phone.java
@@ -28,10 +28,18 @@ public class Phone {
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
         Phone phoneOne = new Phone("Samsung A1",2);
         Phone phoneTwo = new Phone("Samsung A2",4);
         System.out.println("Ez egy " + phoneOne.type + " telefon " + phoneOne.mem + "GB memóriával.");
         System.out.println("Ez egy " + phoneTwo.type + " telefon " + phoneTwo.mem + "GB memóriával.");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Add meg a telefon típusát!");
+        String type = scanner.nextLine();
+        System.out.println("Add meg a memória méretét!");
+        int mem = Integer.parseInt(scanner.nextLine());
+        Phone phoneThree = new Phone(type,mem);
+        System.out.println("Ez egy " + phoneThree.type + " telefon " + phoneThree.mem + "GB memóriával.");
     }
 }
