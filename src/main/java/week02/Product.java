665404efb0952697a9449134ffd4721080f86bd7
Sanyi <hunited@gmail.com>
2020-11-04 11:16:22 +0100

    Product órai munka

---
+++ src/main/java/week02/Product.java
@@ -0,0 +1,35 @@
+package week02;
+
+public class Product {
+    private String name;
+    private String code;
+
+    public Product(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public boolean areTheyEqual(Product p) {
+        if (this.name == p.name) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Product product1 = new Product("alma", "a01");
+        Product product2 = new Product("alma", "a02");
+        Product product3 = new Product("körte", "a03");
+
+        System.out.println(product1.areTheyEqual(product2));
+    }
+}
