659b79c47a928de0e914e439f2b6ac8ce0b7f948
Sanyi <hunited@gmail.com>
2020-11-06 19:21:32 +0100

    Konstansok feladat - Gentleman feltöltése

---
+++ src/main/java/finalmodifier/Gentleman.java
@@ -0,0 +1,19 @@
+package finalmodifier;
+
+public class Gentleman {
+
+    public static final String MESSAGE_PREFIX = "Hello ";
+
+    public String sayHello(String name) {
+        return MESSAGE_PREFIX + name;
+    }
+
+    public static void main(String[] args) {
+
+        Gentleman gentleman = new Gentleman();
+
+        System.out.println(gentleman.sayHello("Sanyi"));
+
+    }
+
+}
