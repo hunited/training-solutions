9d366fd9962f7c93d9af2c560faaa1263c4f8712
Sanyi <hunited@gmail.com>
2020-11-10 10:58:20 +0100

    Keddi konzultáció feladat.

---
+++ src/main/java/week03/Position.java
@@ -0,0 +1,55 @@
+package week03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Position {
+
+    private String name;
+    private int bonus;
+
+    public Position(String name, int bonus) {
+        this.name = name;
+        this.bonus = bonus;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(int bonus) {
+        this.bonus = bonus;
+    }
+
+    @Override
+    public String toString() {
+        return "Position{" +
+                "name='" + name + '\'' +
+                ", bonus=" + bonus +
+                '}';
+    }
+
+    public static void main(String[] args) {
+
+        List<Position> positions = new ArrayList<>();
+
+        positions.add(new Position("Marketing vezető", 500000));
+        positions.add(new Position("HR vezető", 300000));
+        positions.add(new Position("Raktár vezető", 100000));
+
+        for (Position position : positions) {
+            if (position.getBonus() > 150000) {
+                System.out.println(position);
+            }
+        }
+    }
+
+}
