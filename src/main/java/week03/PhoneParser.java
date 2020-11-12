0ff17122c36fb6c6f8dca995a0ec2f172679ceca
Sanyi <hunited@gmail.com>
2020-11-12 11:18:26 +0100

    Week03 csütörtöki órai munka

---
+++ src/main/java/week03/PhoneParser.java
@@ -0,0 +1,28 @@
+package week03;
+
+import java.util.Scanner;
+
+public class PhoneParser {
+
+    public Phone parse(String s) {
+        String prefix = s.substring(0, 2);
+        String number = s.substring(3);
+
+        Phone phone = new Phone(prefix, number);
+        return phone;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String countryPrefix = "+36-";
+        System.out.println("Add meg a telefonszámot a következő formátumban: 30-xxxxxxx");
+
+        PhoneParser phoneParser = new PhoneParser();
+
+        System.out.println("A megadott szám: " + countryPrefix + phoneParser.parse(scanner.nextLine().toString()));
+
+    }
+
+}
---
+++ src/test/java/week03/PhoneParserTest.java
@@ -0,0 +1,25 @@
+package week03;
+
+import introjunit.Gentleman;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PhoneParserTest {
+
+    @Test
+    void testParse() {
+
+        // Given
+        PhoneParser parser = new PhoneParser();
+        String s = "80-6666666";
+
+        // When
+        Phone phone = parser.parse(s);
+
+        // Then
+        assertEquals("80-6666666", phone);
+
+    }
+
+}
