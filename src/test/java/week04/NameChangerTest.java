bbae8d0e14d919144cc4433d0707cf27c74db531
Sanyi <hunited@gmail.com>
2020-11-16 13:58:16 +0100

    Week04 Hétfői feladat

--- src/test/java/week04/NameChangerTest.java
+++ src/test/java/week04/NameChangerTest.java
@@ -1,4 +1,32 @@
 package week04;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class NameChangerTest {
+
+    @Test
+    void testName() {
+
+        assertEquals("John Doe", new NameChanger("John Doe").getFullName());
+
+    }
+
+    @Test
+    void testNameEmpty() {
+        NameChanger nameChanger = new NameChanger("John Doe");
+        assertEquals("John Doe", nameChanger.getFullName());
+
+        IllegalArgumentException iae = assertThrows(IllegalArgumentException.class, () -> new NameChanger(""));
+        assertEquals("Invalid name: ", iae.getMessage());
+    }
+
+    @Test
+    void testNameChanger() {
+
+        assertEquals("Jane Doe", new NameChanger("John Doe").changeFirstName("Jane"));
+
+    }
+
 }
