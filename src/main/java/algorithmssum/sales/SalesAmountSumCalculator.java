266210d3ff207cbc0a5b4251416977c7323ea39d
Sanyi <hunited@gmail.com>
2020-11-16 20:51:11 +0100

    Programozási tételek és egyéb algoritmusok: Összegzés tétele lecke, Salesperson, SalespersonTest, SalesAmountSumCalculator és SalesAmountSumCalculatorTest feladat feltöltése.

---
+++ src/main/java/algorithmssum/sales/SalesAmountSumCalculator.java
@@ -0,0 +1,15 @@
+package algorithmssum.sales;
+
+import java.util.List;
+
+public class SalesAmountSumCalculator {
+
+    public  int sumSalesAmount(List<Salesperson> salespersons) {
+        int sum = 0;
+        for (Salesperson salesperson : salespersons) {
+            sum += salesperson.getAmount();
+        }
+        return sum;
+    }
+
+}
