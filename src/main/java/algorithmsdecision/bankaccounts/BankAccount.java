db2051ead2988c897c3833452436b58891247a88
Sanyi <hunited@gmail.com>
2020-11-17 17:02:02 +0100

    Programozási tételek és egyéb algoritmusok: Eldöntés tétele lecke, Nagy összegű bankszámlák fájljainak a feltöltése.

---
+++ src/main/java/algorithmsdecision/bankaccounts/BankAccount.java
@@ -0,0 +1,42 @@
+package algorithmsdecision.bankaccounts;
+
+public class BankAccount {
+
+    private String nameOfOwner;
+
+    private String accountNumber;
+
+    private int balance;
+
+    public BankAccount(String nameOfOwner, String accountNumber, int balance) {
+        this.nameOfOwner = nameOfOwner;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public boolean withdraw(int amount) {
+        if (balance > amount) {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean deposit(int amount) {
+        balance += amount;
+        return true;
+    }
+
+    public String getNameOfOwner() {
+        return nameOfOwner;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+}
