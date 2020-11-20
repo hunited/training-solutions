ba70e9d5d4ff4f60a08806d39fdb3edd25d2dfe6
Sanyi <hunited@gmail.com>
2020-11-20 19:33:46 +0100

    2. projektmunka V0.4 (29/29 pass - Menü 4 hibás, Menü 5 fejlesztés alatt)

--- src/main/java/schoolrecords/SchoolRecordsController.java
+++ src/main/java/schoolrecords/SchoolRecordsController.java
@@ -1,29 +1,43 @@
 package schoolrecords;
 
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class SchoolRecordsController {
 
-    public static ClassRecords classRecords = new ClassRecords("Fourth Grade A", new Random(5));
+    //public ClassRecords cr = new ClassRecords("Fourth Grade A", new Random(5));
+    public ClassRecords cr;
 
-    public static Scanner scanner = new Scanner(System.in);
+    public SchoolRecordsController(ClassRecords cr) {
+        this.cr = cr;
+    }
+
+    public Scanner scanner = new Scanner(System.in);
 
     private Tutor tutor;
 
     private Subject subject;
 
     public void initSchool() {
-        Tutor tutor1 = new Tutor("Nagy Csilla", Arrays.asList(new Subject("matematika"), new Subject("fizika")));
-        Tutor tutor2 = new Tutor("Gipsz Jakab", Arrays.asList(new Subject("andul"), new Subject("német")));
-        Tutor tutor3 = new Tutor("Teszt Elek", Arrays.asList(new Subject("nyelvtan"), new Subject("irodalom")));
-        Tutor tutor4 = new Tutor("Min Tamás", Arrays.asList(new Subject("kémia"), new Subject("testnevelés")));
+        List<Tutor> tutors = new ArrayList<>();
+        tutors.add(new Tutor("Nagy Csilla", Arrays.asList(new Subject("matematika"), new Subject("fizika"))));
+        tutors.add(new Tutor("Gipsz Jakab", Arrays.asList(new Subject("andul"), new Subject("német"))));
+        tutors.add(new Tutor("Teszt Elek", Arrays.asList(new Subject("nyelvtan"), new Subject("irodalom"))));
+        tutors.add(new Tutor("Min Tamás", Arrays.asList(new Subject("kémia"), new Subject("testnevelés"))));
+
+        List<Subject> subjects = new ArrayList<>();
+        subjects.add(new Subject("matematika"));
+        subjects.add(new Subject("fizika"));
+        subjects.add(new Subject("andul"));
+        subjects.add(new Subject("német"));
+        subjects.add(new Subject("nyelvtan"));
+        subjects.add(new Subject("irodalom"));
+        subjects.add(new Subject("kémia"));
+        subjects.add(new Subject("testnevelés"));
     }
 
-    public static void startMenu() {
+    public void startMenu() {
         System.out.println(
-                classRecords.getClassName() + " osztálynapló. Kérem válasszon funkciót:\n" +
+                cr.getClassName() + " osztálynapló. Kérem válasszon funkciót:\n" +
                         "1. Diákok nevének listázása\n" +
                         "2. Diák név alapján keresése\n" +
                         "3. Diák létrehozása\n" +
@@ -38,67 +52,188 @@ public class SchoolRecordsController {
         );
     }
 
-    public static void runMenu() {
+    public void runMenu() {
         startMenu();
         int selection;
         try {
             selection = Integer.parseInt(scanner.nextLine());
             if (selection == 1) {
-                System.out.println(classRecords.listStudentNames());
-                runMenu();
+                menu1();
             } else if (selection == 2) {
-                System.out.println("Kérem adja meg a keresett diák nevét:");
-                try {
-                    System.out.println(classRecords.findStudentByName(scanner.nextLine()));
-                    runMenu();
-                } catch (Exception e) {
-                    System.out.println("Nincs ilyen nevű gyermek. Újra próbálja? 1: igen, más szám: vissza a menübe.");
-                    int tryAgain = Integer.parseInt(scanner.nextLine());
-                    if (tryAgain == 1) {
-                        System.out.println("Keresendő név:");
-                        System.out.println(classRecords.findStudentByName(scanner.nextLine()));
-                        runMenu();
-                    } else {
-                        runMenu();
-                    }
-                }
+                menu2();
             } else if (selection == 3) {
-                System.out.println("Adja meg az új diák nevét!");
-                try {
-                    classRecords.addStudent(new Student(scanner.nextLine()));
-                } catch (IllegalArgumentException iae) {
-                    System.out.println("Hiba történt. Próbálja újra. " + iae.getMessage());
-                }
-                runMenu();
+                menu3();
             } else if (selection == 4) {
-                runMenu();
+                menu4();
             } else if (selection == 5) {
-                runMenu();
+                menu5();
             } else if (selection == 6) {
-                runMenu();
+                menu6();
             } else if (selection == 7) {
-                runMenu();
+                menu7();
             } else if (selection == 8) {
-                runMenu();
+                menu8();
             } else if (selection == 9) {
-                runMenu();
+                menu9();
             } else if (selection == 10) {
-                runMenu();
+                menu10();
             } else if (selection == 11) {
-                System.out.println("Viszont látásra!");
+                menu11();
                 return;
             } else {
-                System.out.println("Hibás menüválasztás. Próbálja újra.");
-                runMenu();
+                nonValid();
             }
         } catch (IllegalArgumentException iae) {
-            System.out.println("Hiba történt. Próbálja újra. " + iae.getMessage());
-            runMenu();
+            iaError(iae.getMessage());
+        }
+        runMenu();
+    }
+
+    public void findStudent(String whatToDo) {
+        try {
+            System.out.println("Kérem adja meg a " + whatToDo + "diák nevét:");
+            System.out.println(cr.findStudentByName(scanner.nextLine()));
+        } catch (IllegalArgumentException iae) {
+            System.out.println("Nincs ilyen nevű gyermek. Újra próbálja? 1: igen, más szám: vissza a menübe.");
+            int tryAgain = Integer.parseInt(scanner.nextLine());
+            if (tryAgain == 1) {
+                findStudent("");
+            } else {
+                return;
+            }
         }
     }
 
+    public MarkType findMarkType(int sMark){
+        MarkType sMarkType = null;
+        if (MarkType.A.getValue() == sMark) {
+            sMarkType = MarkType.A;
+        } else if (MarkType.B.getValue() == sMark) {
+            sMarkType = MarkType.B;
+        } else if (MarkType.C.getValue() == sMark) {
+            sMarkType = MarkType.C;
+        } else if (MarkType.D.getValue() == sMark) {
+            sMarkType = MarkType.D;
+        } else if (MarkType.F.getValue() == sMark) {
+            sMarkType = MarkType.F;
+        } else {
+            throw new IllegalArgumentException("Nincs ilyen osztályzat");
+        }
+        return sMarkType;
+    }
+
+    public void menu1() {
+        try {
+            System.out.println(cr.listStudentNames());
+        } catch (StringIndexOutOfBoundsException sioobe) {
+            System.out.println("Még nincs diák az osztályban. Használja a 3-as menüpontot.");
+        }
+    }
+
+    public void menu2() {
+        try {
+            if (cr.listStudentNames() != null) {
+                findStudent("keresett ");
+            }
+        } catch (StringIndexOutOfBoundsException sioobe) {
+            System.out.println("Még nincs diák az osztályban. Használja a 3-as menüpontot.");
+        }
+    }
+
+    public void menu3() {
+        System.out.println("Adja meg az új diák nevét!");
+        try {
+            cr.addStudent(new Student(scanner.nextLine()));
+        } catch (IllegalArgumentException iae) {
+            System.out.println("Hiba történt. Próbálja újra. " + iae.getMessage());
+        }
+    }
+
+    public void menu4() {
+        try {
+            findStudent("törlendő ");
+            Student toDel = cr.findStudentByName(scanner.nextLine());
+            cr.removeStudent(toDel);
+            System.out.println("Sikeresen kitöröltük " + toDel.toString() + " nevét a listából.");
+        } catch (IllegalArgumentException iae) {
+            System.out.println("Nincs ilyen nevű gyermek. Újra próbálja? 1: igen, más szám: vissza a menübe.");
+        }
+    }
+
+    public void menu5() {
+        try {
+            Student whoRepetition = cr.repetition();
+            System.out.println("Felel a következő diák: " + whoRepetition.getName());
+            System.out.println("Kapott osztályzat: ");
+            MarkType sMarkType = findMarkType(Integer.parseInt(scanner.nextLine()));
+            System.out.println("Tantárgy neve: ");
+            Subject sSubject = new Subject(scanner.nextLine());
+            System.out.println("Tanár neve: ");
+            String sTeacher = scanner.nextLine();
+
+            whoRepetition.grading(new Mark(sMarkType, sSubject, new Tutor(sTeacher, Arrays.asList(sSubject))));
+        } catch (Exception e) { //Milyen kivétel?
+
+        }
+    }
+
+    public void menu6() {
+        try {
+
+        } catch (Exception e) { //Milyen kivétel?
+
+        }
+    }
+
+    public void menu7() {
+        try {
+
+        } catch (Exception e) { //Milyen kivétel?
+
+        }
+    }
+
+    public void menu8() {
+        try {
+
+        } catch (Exception e) { //Milyen kivétel?
+
+        }
+    }
+
+    public void menu9() {
+        try {
+
+        } catch (Exception e) { //Milyen kivétel?
+
+        }
+    }
+
+    public void menu10() {
+        try {
+
+        } catch (Exception e) { //Milyen kivétel?
+
+        }
+    }
+
+    public void menu11() {
+        System.out.println("Viszont látásra!");
+    }
+
+    public void nonValid() {
+        System.out.println("Hibás menüválasztás. Próbálja újra.");
+    }
+
+    public void iaError(String msg) {
+        System.out.println("Hiba történt. Próbálja újra. " + msg);
+    }
+
     public static void main(String[] args) {
-        runMenu();
+
+        SchoolRecordsController src = new SchoolRecordsController(new ClassRecords("Fourth Grade A", new Random(5)));
+        src.runMenu();
+
     }
 
 }
