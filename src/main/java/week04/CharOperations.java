2661c93e0c1eb18bc8cb641de2143ff6594c35a8
Sanyi <hunited@gmail.com>
2020-11-20 11:02:54 +0100

    Órai munka 4. hét péntek

--- src/main/java/week04/CharOperations.java
+++ src/main/java/week04/CharOperations.java
@@ -8,7 +8,7 @@ public class CharOperations {
     public List<Integer> getPositionOfChar(String base, String part) {
         List<Integer> indexes = new ArrayList<>();
         for (int i = 0; i < base.length(); i++) {
-            if (part.equals(base.charAt(i))) {
+            if (part.toLowerCase().charAt(0) == base.toLowerCase().charAt(i)) {
                 indexes.add(i);
             }
         }
