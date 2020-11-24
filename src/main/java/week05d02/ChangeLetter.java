32e8e8e218dbe0ac4a1949480ae95e79c780ff9e
Sanyi <hunited@gmail.com>
2020-11-24 10:39:12 +0100

    Órai munka, nem fut.

--- src/main/java/week05d02/ChangeLetter.java
+++ src/main/java/week05d02/ChangeLetter.java
@@ -11,10 +11,10 @@ public class ChangeLetter {
             for (int j = 0; j < characters.size(); j++) {
                 if (string.toLowerCase().charAt(i) == characters.get(j)) {
                     string.replace(string.charAt(i), '*');
-                    System.out.println(i);
+                    System.out.println(string.charAt(i));
                 }
                 else {
-                    System.out.println(characters.get(j) + " nem található");
+                    //System.out.println(characters.get(j) + " nem található");
                 }
             }
         }
