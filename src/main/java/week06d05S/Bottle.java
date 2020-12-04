52de7979aac4548ed79cd231ea152034653333ae
Sanyi <hunited@gmail.com>
2020-12-04 10:41:05 +0100

    Órai munka, Week06Day05S Senior Init version

---
+++ src/main/java/week06d05S/Bottle.java
@@ -0,0 +1,43 @@
+package week06d05S;
+
+public class Bottle {
+
+    private BottleType type;
+    private int filledUnit;
+
+    public Bottle(BottleType type, int filledUnit) {
+        this.type = type;
+        if (filledUnit < 1) {
+            throw new IllegalArgumentException("A mennyiség nem lehet egynél kevesebb!");
+        }
+        this.filledUnit = filledUnit;
+    }
+
+    public static Bottle of(BottleType bottleType) {
+        return new Bottle(bottleType, 0);
+    }
+
+    public void fill(int fillAmount) {
+        this.filledUnit += fillAmount;
+        if (filledUnit > this.type.getMaximumAmount()) {
+            throw new IllegalArgumentException("Túlcsordult!");
+        }
+    }
+
+    public BottleType getType() {
+        return type;
+    }
+
+    public int getFilledUnit() {
+        return filledUnit;
+    }
+
+    public static void main(String[] args) {
+        Bottle b = new Bottle(BottleType.GLASS_BOTTLE, 250);
+        System.out.println(b.getFilledUnit());
+        b.fill(50);
+        System.out.println(b.getFilledUnit());
+        b.fill(1);
+        System.out.println(b.getFilledUnit());
+    }
+}
---
+++ src/test/java/week06d05S/BottleTest.java
@@ -0,0 +1,29 @@
+package week06d05S;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class BottleTest {
+
+    @Test
+    public void tesztBottle() {
+        Bottle b = new Bottle(BottleType.GLASS_BOTTLE, 250);
+        assertEquals(250, b.getFilledUnit());
+    }
+
+    @Test
+    public void tesztBottleFill() {
+        Bottle b = new Bottle(BottleType.GLASS_BOTTLE, 250);
+        b.fill(50);
+        assertEquals(300, b.getFilledUnit());
+    }
+
+    @Test
+    public void tesztBottleErr() {
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> new Bottle(BottleType.GLASS_BOTTLE, 300).fill(1));
+        assertEquals("Túlcsordult!", ex.getMessage());
+    }
+
+}
