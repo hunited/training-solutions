f9036adf4aab9c55413e17b40b68fb29d1f2ef28
Sanyi <hunited@gmail.com>
2020-12-08 10:25:39 +0100

    Week07D02S Init

---
+++ src/main/java/week07d02S/User.java
@@ -0,0 +1,10 @@
+package week07d02S;
+
+public interface User {
+
+    public String getUsername();
+    public String getFirstName();
+    public String getLastName();
+    public String getFullName();
+
+}
