704e6999db6f77d9de9725ddc1d7ef3d8e8c128e
Sanyi <hunited@gmail.com>
2020-12-09 19:38:20 +0100

    Öröklődés: Metódusok öröklődése lecke, Gyakorlat - Product és PackedProduct osztályok feladat fájljainak a feltöltése.

---
+++ src/main/java/inheritancemethods/products/Product.java
@@ -0,0 +1,38 @@
+package inheritancemethods.products;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Product {
+
+    private String name;
+    private BigDecimal unitWeight;
+    private int numberOfDecimals;
+
+    public Product(String name, BigDecimal unitWeight, int numberOfDecimals) {
+        this.name = name;
+        this.unitWeight = unitWeight;
+        this.numberOfDecimals = numberOfDecimals;
+    }
+
+    public Product(String name, BigDecimal unitWeight) {
+        this(name, unitWeight, 2);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getUnitWeight() {
+        return unitWeight;
+    }
+
+    public int getNumberOfDecimals() {
+        return numberOfDecimals;
+    }
+
+    public BigDecimal totalWeight(int pieces) {
+        return unitWeight.multiply(new BigDecimal(String.valueOf(pieces))).setScale(numberOfDecimals, RoundingMode.HALF_UP);
+    }
+
+}
