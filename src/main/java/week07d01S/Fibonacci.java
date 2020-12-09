137f673a41edcb939032cd868ed67d7bfb9c8d3a
Sanyi <hunited@gmail.com>
2020-12-09 13:30:20 +0100

    Órai munka, Week07Day01S Init version

---
+++ src/main/java/week07d01S/Fibonacci.java
@@ -0,0 +1,20 @@
+package week07d01S;
+
+public class Fibonacci {
+
+    public static long fib(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Counter can't be less then 0");
+        } else if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        }
+        return fib(n - 2) + fib(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fib(25));
+    }
+
+}
