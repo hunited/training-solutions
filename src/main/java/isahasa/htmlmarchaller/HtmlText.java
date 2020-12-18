629f097e7526132cd0065f0d5888718b811955a7
Sanyi <hunited@gmail.com>
2020-12-18 17:57:46 +0100

    Haladó OO elvek: is-a has-a kapcsolatok lecke, Html dekorátor feladat fájljai.

---
+++ src/main/java/isahasa/htmlmarchaller/HtmlText.java
@@ -0,0 +1,15 @@
+package isahasa.htmlmarchaller;
+
+public class HtmlText implements TextSource {
+
+    private final String plainText;
+
+    public HtmlText(String plainText) {
+        this.plainText = plainText;
+    }
+
+    @Override
+    public String getPlainText() {
+        return plainText;
+    }
+}
---
+++ src/main/java/isahasa/htmlmarchaller/hasa/TextDecorator.java
@@ -0,0 +1,17 @@
+package isahasa.htmlmarchaller.hasa;
+
+import isahasa.htmlmarchaller.TextSource;
+
+public abstract class TextDecorator implements TextSource {
+
+    private TextSource textSource;
+
+    public TextDecorator(TextSource textSource) {
+        this.textSource = textSource;
+    }
+
+    public TextSource getTextSource() {
+        return textSource;
+    }
+
+}
