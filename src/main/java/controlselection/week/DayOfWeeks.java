00c43da046be05f60b036403a96aa5d70eea4152
Sanyi <hunited@gmail.com>
2020-12-21 19:29:23 +0100

    Vezérlési szerkezetek és az elágazás lecke Hét napjai feladat.

---
+++ src/main/java/controlselection/week/DayOfWeeks.java
@@ -0,0 +1,15 @@
+package controlselection.week;
+
+public class DayOfWeeks {
+
+    public String whichDay(String day) {
+        switch (day.toLowerCase()) {
+            case "hétfő" : return "hét eleje";
+            case "kedd", "szerda", "csütörtök" : return "hét közepe";
+            case "péntek" : return "majdnem hétvége";
+            case "szombat", "vasárnap" : return "hét vége";
+            default: throw new IllegalArgumentException("Invalid day: " + day);
+        }
+    }
+
+}
