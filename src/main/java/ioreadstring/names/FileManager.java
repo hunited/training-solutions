18c0531240944ade04385119ecc4ed844aae44a2
Sanyi <hunited@gmail.com>
2021-01-06 10:43:13 +0100

    Java SE haladó - Kivétel- és fájlkezelés: Szöveges állomány beolvasása lecke, Emberek feladat fájljai.

---
+++ src/main/java/ioreadstring/names/FileManager.java
@@ -0,0 +1,40 @@
+package ioreadstring.names;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileManager {
+
+    private Path myFile;
+    private List<Human> humans = new ArrayList<>();
+
+    public FileManager(String path) {
+        this.myFile = Path.of(path);
+    }
+
+    public void readFromFile() {
+        try {
+            List<String> names = Files.readAllLines(myFile, StandardCharsets.UTF_8);
+            for (String name : names) {
+                String[] row = name.split(" ");
+                Human human = new Human(row[0], row[1]);
+                humans.add(human);
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not read file!", ioe);
+        }
+    }
+
+    public Path getMyFile() {
+        return myFile;
+    }
+
+    public List<Human> getHumans() {
+        return new ArrayList<>(humans);
+    }
+
+}
