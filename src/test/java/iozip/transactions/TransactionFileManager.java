efd9ebe2b79e0093cd4a97d091d03f6a27f3a558
Sanyi <hunited@gmail.com>
2021-02-01 18:10:15 +0100

    Java SE haladó - Kivétel- és fájlkezelés: Bájtok írása tömörítéssel lecke, Napi tranzakciók feladat fájljai.

---
+++ src/test/java/iozip/transactions/TransactionFileManager.java
@@ -0,0 +1,23 @@
+package iozip.transactions;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class TransactionFileManager {
+    public void saveTransactions(Path path, List<Transaction> transactions) {
+        try (ZipOutputStream os = new ZipOutputStream(new BufferedOutputStream(Files.newOutputStream(path)))) {
+            for (Transaction transaction : transactions) {
+                os.putNextEntry(new ZipEntry(Long.toString(transaction.getId())));
+                String line = transaction.getTime().toString() + "\n" + transaction.getAccount() + "\n" + transaction.getAmount();
+                os.write(line.getBytes());
+            }
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Can not write file", ioe);
+        }
+    }
+}
