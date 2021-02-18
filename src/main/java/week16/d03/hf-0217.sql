6d72425e9aea4b87b43bb051bfe878ec86aad386
Sanyi <hunited@gmail.com>
2021-02-18 11:49:28 +0100

    Órai munka, házi feladat mókolások, Week16 Day03

--- src/main/java/week16/d03/hf-0217.sql
+++ src/main/java/week16/d03/hf-0217.sql
@@ -1,9 +1,20 @@
+--Listázd ki az összes vásárlót név szerint csökkenősorrendben!
 SELECT * FROM `customers` WHERE 1 ORDER BY `customer_name` DESC;
+--Listázd ki az összes vásárlót, de csak a nevüket, az azonosítójuk nem kell.
 SELECT `customer_name` FROM `customers` WHERE 1;
+--Listázd ki a pizzákat áruk szerint emelkedősorrendben!
 SELECT * FROM `pizza` WHERE 1 order BY `pizza_price` ASC;
-SELECT *, MAX(`pizza_price`) FROM `pizza`;
+--Listázd ki a legdrágább pizzát!
+SELECT * FROM `pizza` ORDER BY `pizza_price` DESC LIMIT 1;
+--vagy
+SELECT * FROM `pizza` WHERE `pizza_price` = (SELECT MAX(`pizza_price`) FROM `pizza`);
+--Átlagosan mekkora értékben rendeltek az emberek?
 SELECT AVG(`pizza_price`) AS 'Átlag pizzaár' FROM `pizza` INNER JOIN `order` ON `order`.`pizza_id` = `pizza`.`pizza_id`;
+--Számold meg, mennyi vásárló van, akinek a nevében van ’a’ betű!
 SELECT * FROM `customers` WHERE `customer_name` LIKE '%a%';
+--Listázd ki azokat a pizzákat, amik 1000 forintnál drágábbak vagy van a nevükben ’a’ betű.
 SELECT * FROM `pizza` WHERE `pizza_price` > 1000 OR `pizza_name` LIKE '%a%';
-SELECT `customers`.`customer_name` AS 'Vevő', `order`.`order_date` AS 'Dátum' FROM `order` INNER JOIN `customers` ON `customers`.`customer_id` = `order`.`customer_id`;
+--Listázd ki az emberek neveit, akik pizzát rendeltek, és azt, hogy mikor rendelték a pizzát.
+SELECT `customers`.`customer_name` AS 'Vevő', `order`.`order_date` AS 'Dátum' FROM `order` LEFT JOIN `customers` ON `customers`.`customer_id` = `order`.`customer_id`;
+--Listázd ki a pizzarendelések dátumát és azt, hogy melyik dátumon milyen nevűpizzát rendeltek!
 SELECT `order`.`order_date` AS 'Dátum', `pizza`.`pizza_name` AS 'Pizza neve' FROM `order` INNER JOIN `pizza` ON `order`.`pizza_id` = `pizza`.`pizza_id`;
--- src/main/java/week16/d03/om0217.sql
+++ src/main/java/week16/d03/om0217.sql
@@ -1,12 +1,24 @@
-SELECT `forvaros` FROM `orszagok` WHERE `orszag` = 'Madagaszkár';
-SELECT `orszag` FROM `orszagok` WHERE `forvaros` = 'Ouagadougou';
+-- 1.  Mi MADAGASZKÁR fővárosa?
+SELECT `fovaros` FROM `orszagok` WHERE `orszag` = 'Madagaszkár';
+-- 2.  Melyik ország fővárosa OUAGADOUGOU?
+SELECT `orszag` FROM `orszagok` WHERE `fovaros` = 'Ouagadougou';
+-- 3.  Melyik ország autójele a TT?
 SELECT `orszag` FROM `orszagok` WHERE `autojel` = 'TT';
+-- 4.  Melyik ország pénzének jele az SGD?
 SELECT `orszag` FROM `orszagok` WHERE `penzjel` = 'SGD';
+-- 5.  Melyik ország nemzetközi telefon-hívószáma a 61?
 SELECT `orszag` FROM `orszagok` WHERE `telefon` = 61;
+-- 6.  Mekkora területű Monaco?
 SELECT `terulet` FROM `orszagok` WHERE `orszag` = 'Monaco';
+-- 7.  Hányan laknak Máltán?
 SELECT `nepesseg` FROM `orszagok` WHERE `orszag` = 'Málta';
+-- 8.  Mennyi Japán népsűrűsége?
 SELECT `nepesseg` / `terulet` * 1000 FROM `orszagok` WHERE `orszag` = 'Japán';
+-- 9.  Mely országok népsűrűsége nagyobb 500 fő/km2-nél?
 SELECT `orszag` FROM `orszagok` WHERE `nepesseg` / `terulet` * 1000 > 500;
+-- 10.  Melyek a szigetországok?
 SELECT `orszag` FROM `orszagok` WHERE `foldr_hely` LIKE '%Szigetország%';
+-- 11.  Melyik a első hat legnagyobb területű ország?
 SELECT `orszag` FROM `orszagok` ORDER BY `terulet` DESC LIMIT 6;
+-- 12.  Melyik három ország területe a legkisebb?
 SELECT `orszag` FROM `orszagok` ORDER BY `terulet` ASC LIMIT 3;
