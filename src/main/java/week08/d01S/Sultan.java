bd583bea17f805db33c4de19f06d1d2efca34455
Sanyi <hunited@gmail.com>
2021-03-09 14:23:27 +0100

    Órai munka, Week08 Day01 Senior Updated version

--- src/main/java/week08/d01S/Sultan.java
+++ src/main/java/week08/d01S/Sultan.java
@@ -5,18 +5,18 @@ import java.util.List;
 
 public class Sultan {
 
-    private static final int DAYS = 100;
-    private final boolean[] prisonCells = new boolean[DAYS];
+    private static final int DAYS_AND_CELLS = 100;
+    private final boolean[] prisonCells = new boolean[DAYS_AND_CELLS];
 
     public Sultan() {
-        for (int i = 0; i < DAYS; i++) {
+        for (int i = 0; i < DAYS_AND_CELLS; i++) {
             prisonCells[i] = false;
         }
     }
 
     public List<Integer> openedDoors() {
         List<Integer> openedDoors = new ArrayList<>();
-        for (int i = 0; i < DAYS; i++) {
+        for (int i = 0; i < DAYS_AND_CELLS; i++) {
             if (getCellOpenStatus(i)) {
                 openedDoors.add(i + 1);
             }
@@ -25,7 +25,7 @@ public class Sultan {
     }
 
     private boolean getCellOpenStatus(int i) {
-        for (int j = 0; j < DAYS; j++) {
+        for (int j = i; j < DAYS_AND_CELLS; j++) {
             if ((j + 1) % (i + 1) == 0) {
                 prisonCells[j] = !prisonCells[j];
             }
